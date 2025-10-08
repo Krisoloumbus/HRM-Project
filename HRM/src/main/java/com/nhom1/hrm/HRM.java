38a786662b8823e4e390f08046c4f05394cb744a
Krisoloumbus <tuankhanhtran520@gmail.com>
2025-10-08 23:00:07 +0700

    Adding HRM project to git

---
+++ HRM/src/main/java/com/nhom1/hrm/HRM.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.nhom1.hrm;
+
+/**
+ *
+ * @author Kris
+ */
+public class HRM {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
