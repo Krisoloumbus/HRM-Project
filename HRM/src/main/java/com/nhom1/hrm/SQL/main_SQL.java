913aadd2f07c396e3e50fe82d1f013e9b28d0112
Krisoloumbus <tuankhanhtran520@gmail.com>
2025-10-19 15:25:09 +0700

    Update SQL folder with file: table, main_SQL

---
+++ HRM/src/main/java/com/nhom1/hrm/SQL/main_SQL.java
@@ -0,0 +1,21 @@
+package com.nhom1.hrm.SQL;
+
+import java.sql.Connection;
+
+public class main_SQL {
+    public static void main(String[] args) {
+        try {
+            Connection conn = testconnectsql.getConnection();
+            System.out.println("Connected");
+            try {
+                table.taobangifchuaco(conn);
+                System.out.println("Table created");
+            } catch (Exception e1) {
+                System.out.println("Failed to create table");
+                e1.printStackTrace();}
+        } catch (Exception e2) {
+            System.out.println("Connect failed");
+            e2.printStackTrace();
+        }
+    }
+}
---
+++ HRM/src/main/java/com/nhom1/hrm/SQL/table.java
@@ -0,0 +1,33 @@
+import java.sql.Connection;
+import java.sql.Statement;
+
+public class table {
+        private static final String taobang ="""
+        IF NOT EXISTS (
+            SELECT 1
+            FROM sys.tables t
+            JOIN sys.schemas s ON s.schema_id = t.schema_id
+            WHERE t.name = 'Employees' AND s.name = 'dbo'
+        )
+        BEGIN
+            CREATE TABLE dbo.Employees(
+                No INT IDENTITY(1,1) CONSTRAINT PK_Employees PRIMARY KEY,
+                EID AS ('HRM' + RIGHT('00000' + CAST(No AS VARCHAR(5)), 5)) PERSISTED UNIQUE,
+                Full_Name NVARCHAR(100) NOT NULL,
+                Phone NUMERIC(10,0) NOT NULL,
+                Email VARCHAR(255) UNIQUE,
+                Education VARCHAR(50) NOT NULL,
+                Department VARCHAR(50) NOT NULL,
+                Level VARCHAR(50) NOT NULL,
+                Salary MONEY NOT NULL,
+                CreatedAt DATETIME2 DEFAULT SYSUTCDATETIME()
+            );
+        END
+                """;
+        public static void taobangifchuaco (Connection conn)
+        throws Exception {
+            try (Statement st = conn.createStatement()){
+                st.execute(taobang);
+            }
+        }
+    }
